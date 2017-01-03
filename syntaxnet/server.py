32d4f681b828ff16645d7821e5ea9dbbf81274a4
Erik <erik.langenborg@gmail.com>
2017-01-02 22:52:14 -0500

    Dockerfile to buidl syntaxnet with small web wrapper

---
+++ syntaxnet/server.py
@@ -0,0 +1,18 @@
+from flask import Flask, request
+from subprocess import call
+
+app = Flask(__name__)
+
+@app.route('/text', methods=["POST"])
+def main_text():
+    with open("/root/input.txt", "w") as text_file:
+        text_file.write(request.data)
+    process_results = subprocess.Popen("sh /root/work.sh", shell=True, cwd="/opt/tensorflow/models/syntaxnet")
+    final_results = open('/root/output.conll', 'r').read()
+    return final_results
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return 'This server only deals with POST requests at /text'
+
+app.run(host='0.0.0.0', port=5005)
