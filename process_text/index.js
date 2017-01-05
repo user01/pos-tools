ac8287699b74674f264d59a792949e15e2ee10c2
Erik <erik.langenborg@gmail.com>
2017-01-04 20:04:34 -0500

    Cleaned up full flow

--- process_text/index.js
+++ process_text/index.js
@@ -8,7 +8,6 @@ var text_conll = fs.readFileSync('text.conll', 'utf8').toString();
 
 
 var clean_text_lines = R.pipe(
-  // R.replace(/:\n\n/g, ', '),
   R.split('\n'),
   R.map(
     R.pipe(
@@ -82,7 +81,7 @@ var conll_text_same = ([text, conll]) => {
       clean_str_to_chars
     )(text)
   );
-}
+};
 
 var report_from_lines_objs = (lines, objs, omissions = []) => {
   // console.log(lines.length, objs.length, omissions.length);
@@ -119,10 +118,10 @@ var report_from_lines_objs = (lines, objs, omissions = []) => {
 
 
 
-var processText = (text, onComplete) => {
+var processText = (text, onComplete, warnOnOmissions = true) => {
   request.post({
     url: 'http://localhost:5005/text',
-    // timeout: 60 * 20 * 1000, // milliseconds of timeout
+    timeout: 60 * 20 * 1000, // milliseconds of timeout
     body: clean_text_str(text)
   }, (error, response, body) => {
     if (error || response.statusCode != 200) {
@@ -132,170 +131,24 @@ var processText = (text, onComplete) => {
       console.error(error);
       return;
     }
-    // console.log(body)
     const lines = clean_text_lines(text);
     const objs = conllu_file_to_objs(body);
-    if (lines.length != objs.length) {
-      console.warn(`Length mismatch with text (${lines.length}) and pos (${objs.length})`);
+    const [lines_final, objs_final, omissions] = report_from_lines_objs(lines, objs, []);
+    if (omissions.length > 0) {
+      R.pipe(
+        R.map(R.prop('line')),
+        R.join(', '),
+        t => console.warn(`Warning: ${omissions.length} lines were omitted from final results: ${t}`)
+      )(omissions);
     }
-    onComplete(lines, objs, null)
-    // const [lines_final, objs_final, omissions] = report_from_lines_objs(lines, objs, []);
-    // onComplete(lines_final, objs_final, omissions);
+    onComplete(lines_final, objs_final, omissions);
   });
-}
+};
 
 var t, c, o;
-processText(text_md, (text, conllu, omissions) => {
+processText(text_md.substring(0,5000), (text, conllu, omissions) => {
   t = text;
   c = conllu;
   o = omissions;
   console.log('done');
 });
-
-
-
-// R.map(conll_text_same, R.zip(t,c))
-// t.length
-// c.length
-
-var [lines_final, objs_final, omissions] = report_from_lines_objs(t, c, []);
-
-R.pipe(
-  R.apply(R.zip),
-  // R.nth(500)
-  R.takeLast(1),
-  R.nth(0),
-  R.nth(0),
-  R.nth(0)
-)([lines_final, objs_final]);
-R.pipe(
-  R.apply(R.zip),
-  // R.nth(500)
-  R.takeLast(1),
-  R.nth(0),
-  R.nth(1)
-)([lines_final, objs_final]);
-
-// processText(`
-// “Believe me, my dear Miss Elizabeth, that your modesty, so far from doing you any disservice, rather adds to your other perfections. You would have been less amiable in my eyes had there _not_ been this little unwillingness; but allow me to assure you, that I have your respected mother’s permission for this address. You can hardly doubt the purport of my discourse, however your natural delicacy may lead you to dissemble; my attentions have been too marked to be mistaken. Almost as soon as I entered the house, I singled you out as the companion of my future life. But before I am run away with by my feelings on this subject, perhaps it would be advisable for me to state my reasons for marrying--and, moreover, for coming into Hertfordshire with the design of selecting a wife, as I certainly did.”
-// `,
-//   (text, conllu) => {
-//     console.log('text', text);
-//     console.log('conllu', conllu);
-//   })
-
-
-
-
-
-
-var tryLine = (data_conll, data_text, idx = 0) => {
-  const conll = R.pipe(
-    R.nth(idx),
-    R.map(R.nth(1)),
-    R.join(' ')
-  )(data_conll);
-
-  const text_ = R.pipe(
-    R.filter(x => x.length > 0),
-    R.nth(idx)
-  )(data_text);
-
-  if (!R.is(String, data_conll)) {
-    console.error(data_conll);
-  }
-  const conll_list = clean_str_to_chars(conll);
-  const text_list = clean_str_to_chars(text_);
-  const match = R.equals(conll_list, text_list);
-
-  return {
-    idx,
-    text_,
-    conll,
-    match
-  }
-}
-
-var tryPair = (data_conll, data_text, idx = 0) => {
-  const conll = R.pipe(
-    // R.nth(idx),
-    // R.map(R.nth(1)),
-    R.join(' ')
-  )(data_conll);
-
-  const text_ = R.pipe(
-    // R.filter(x => x.length > 0),
-    // R.nth(idx)
-    R.identity
-  )(data_text);
-
-  const conll_list = clean_str_to_chars(conll);
-  const text_list = clean_str_to_chars(text_);
-  const match = R.equals(conll_list, text_list);
-
-  return {
-    idx,
-    text_,
-    conll,
-    match
-  }
-}
-
-
-
-var first_bad_line = (cc, tt) => {
-  const min_length = R.min(cc.length, tt.length);
-  const pairs = R.zip(
-    R.take(min_length, cc),
-    R.take(min_length, tt)
-  );
-  console.log(`Length ${min_length}`);
-
-  return R.find(([conll, text], idx) => {
-    return !R.equals(
-      R.pipe(
-        R.map(R.nth(1)),
-        R.join(' '),
-        clean_str_to_chars
-      )(conll),
-      clean_str_to_chars(text)
-    );
-  }, pairs);
-}
-
-first_bad_line(c, t)
-
-// tryLine(text_conll, text_md, 350)
-var try_lines = (start, end, step) => {
-  return R.map(
-    i => tryLine(c, t, i),
-    R.pipe(
-      R.range(start),
-      R.map(R.multiply(step))
-    )(end)
-  )
-}
-try_lines(40, 90, 10)
-try_lines(790, 810, 1)
-
-
-
-
-
-
-
-
-
-conlluEntryToObj(text_conll.split('\n\n')[1000])
-
-// text_md.split('\n').filter(x => x.length < 1).length
-// text_md.split('\n').filter(x => x.length > 0)[0]
-// text_md.split('\n').filter(x => x.length > 0)[4]
-text_md.split('\n').filter(x => x.length > 0)[1000]
-text_md.split('\n').filter(x => x.length > 0)[999]
-
-// text_conll.split('\n\n').length
-// text_conll.split('\n\n')[0]
-// text_conll.split('\n\n')[4]
-text_conll.split('\n\n')[1000].split('\n')
-conlluEntryToObj(text_conll.split('\n\n')[1000])[0]
