6e255b31867ba4398d7d58d9dcb8a8c363dc1e0e
Erik <erik.langenborg@gmail.com>
2017-01-05 22:54:42 -0500

    Added styles to markup

--- frontend/index.js
+++ frontend/index.js
@@ -20,7 +20,40 @@ const conll_arr_to_str = R.pipe(
   R.append(''),
   R.join('\n')
 );
-
+const conll_arr_to_styles = R.pipe(
+  R.addIndex(R.map)((elm, idx) => {
+    const id = elm[3];
+    return `# visual-style ${idx + 1}	bgColor:${conll_to_style(id,false)}
+# visual-style ${idx + 1}	fgColor:${conll_to_style(id,true)}`;
+  }),
+  R.join('\n')
+);
+const conll_to_style = (id, foreground) => {
+  switch (id) {
+    case 'VERB':
+      return foreground ? 'white' : '#33a02c';
+    case 'PRON':
+      return foreground ? 'black' : 'pink';
+    case 'DET':
+      return foreground ? 'black' : '#ff7f00';
+    case 'NOUN':
+      return foreground ? 'white' : '#e31a1c';
+    case 'ADV':
+      return foreground ? 'black' : '#b2df8a';
+    case '.':
+      return foreground ? 'white' : 'brown';
+    case 'ADP':
+      return foreground ? 'white' : '#984ea3';
+    case 'ADJ':
+      return foreground ? 'white' : '#1f78b4';
+    case 'NUM':
+      return foreground ? 'black' : '#cab2d6';
+    case 'CONJ':
+      return foreground ? 'black' : '#fdbf6f';
+    default:
+  }
+  return foreground ? 'yellow' : 'pink';
+};
 
 $.ajax({
   url: 'text.json',
@@ -29,14 +62,16 @@ $.ajax({
 
     $('.line').click((elm) => {
 
-      const payload = R.pipe(
+      const conll_arr = R.pipe(
         parseInt,
         R.nth(R.__, data),
-        R.nth(2),
-        conll_arr_to_str
+        R.nth(2)
       )($(elm.srcElement).attr('index'));
 
-      $('#input').val(payload);
+      const payload = conll_arr_to_str(conll_arr);
+      const header = conll_arr_to_styles(conll_arr);
+
+      $('#input').val(`${header}\n${payload}`);
       $('#input').trigger('keyup');
     });
 
